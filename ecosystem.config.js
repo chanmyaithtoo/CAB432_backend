bec88f355b9e07ee1cc46618788b3726596782a8
chanmyaithtoo <htoochan541@gmail.com>
2023-11-02 11:40:16 +1000

    Delete Utilities and modify server.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,14 +8,17 @@ module.exports = {
       "QUT_USER": "n11178931@qut.edu.au",
       "AWS_BUCKET": "n11178931-drive",
       "AWS_REGION": "ap-southeast-2",
-      "JWT_SECRET": "Pr0fessi0nal"
+      "JWT_SECRET": "Pr0fessi0nal",
+      "SQS_QUEUE_URL": "https://sqs.ap-southeast-2.amazonaws.com/901444280953/N11178931_Queue"
     },
     env_production: {
       "NODE_ENV": "production",
       "QUT_USER": "n11178931@qut.edu.au",
       "AWS_BUCKET": "n11178931-drive",
       "AWS_REGION": "ap-southeast-2",
-      "JWT_SECRET": "Pr0fessi0nal"
+      "JWT_SECRET": "Pr0fessi0nal",
+      "SQS_QUEUE_URL": "https://sqs.ap-southeast-2.amazonaws.com/901444280953/N11178931_Queue"
+
     }
   }]
 }
--- server.js
+++ server.js
@@ -9,8 +9,11 @@ const multer = require("multer");
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 const app = express();
-const { compressFiles, getUniqueFileName } = require("./utilities");
+const { compressFiles, getUniqueFileName } = require("../SQS/utilities");
 const { check, validationResult } = require("express-validator");
+const SQS = new AWS.SQS();
+const QUEUE_URL = process.env.SQS_QUEUE_URL; 
+
 
 app.use(express.json());
 app.use(cors());
@@ -20,6 +23,9 @@ const qutUsername = process.env.QUT_USER;
 
 AWS.config.update({
   region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  sessionToken: process.env.AWS_SESSION_TOKEN
 });
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
@@ -113,50 +119,95 @@ const uploadValidations = [
   //   .withMessage("File name should end with .zip or .tar"),
 ];
 
-app.post(
-  "/upload",
-  upload.array("files"),
-  uploadValidations,
+// app.post(
+//   "/upload",
+//   upload.array("files"),
+//   uploadValidations,
 
-  async (req, res) => {
+//   async (req, res) => {
     
-    // if (!errors.isEmpty()) {
-    //     return res.status(400).json({ errors: errors.array() });
-    // }
-    const errors = validationResult(req);
+//     // if (!errors.isEmpty()) {
+//     //     return res.status(400).json({ errors: errors.array() });
+//     // }
+//     const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      console.log("Validation errors:", errors.array()); // Log the errors
-      return res.status(400).json({ errors: errors.array() });
-    }
+//     if (!errors.isEmpty()) {
+//       console.log("Validation errors:", errors.array()); // Log the errors
+//       return res.status(400).json({ errors: errors.array() });
+//     }
 
-    if (!req.files || req.files.length === 0) {
-      return res.status(400).send("No files uploaded.");
-    }
+//     if (!req.files || req.files.length === 0) {
+//       return res.status(400).send("No files uploaded.");
+//     }
 
-    try {
-      const compressedBuffer = await compressFiles(req.files);
-      const dirName = `${req.body.username}_uploads`;
-      let desiredFileName = req.body.desiredFileName;
-      const uniqueFileName = await getUniqueFileName(desiredFileName);
+//     try {
+//       const compressedBuffer = await compressFiles(req.files);
+//       const dirName = `${req.body.username}_uploads`;
+//       let desiredFileName = req.body.desiredFileName;
+//       const uniqueFileName = await getUniqueFileName(desiredFileName);
 
-      const params = {
-        Bucket: process.env.AWS_BUCKET,
-        Key: `${dirName}/${uniqueFileName}`,
-        Body: compressedBuffer,
-      };
+//       const params = {
+//         Bucket: process.env.AWS_BUCKET,
+//         Key: `${dirName}/${uniqueFileName}`,
+//         Body: compressedBuffer,
+//       };
 
-      const uploadedData = await S3.upload(params).promise();
+//       const uploadedData = await S3.upload(params).promise();
 
-      res.status(200).send("Successfully Compressed and Uploaded");
-    } catch (error) {
-      console.error("Error:", error);
-      res.status(500).send("Server error.");
-    }
+//       res.status(200).send("Successfully Compressed and Uploaded");
+//     } catch (error) {
+//       console.error("Error:", error);
+//       res.status(500).send("Server error.");
+//     }
+//   }
+// );
+
+async function uploadToTemporaryS3(file) {
+  const filePath = `TemporaryFiles/${file.originalname}`;
+  await S3.upload({
+    Bucket: process.env.AWS_BUCKET,
+    Key: filePath,
+    Body: file.buffer
+  }).promise();
+  return filePath;
+}
+
+app.post("/upload", upload.array("files"), uploadValidations, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log("Validation errors:", errors.array());
+    return res.status(400).json({ errors: errors.array() });
   }
-);
 
-const PORT = process.env.PORT || 3000;
+  if (!req.files || req.files.length === 0) {
+    return res.status(400).send("No files uploaded.");
+  }
+
+  try {
+    const fileReferences = await Promise.all(req.files.map(uploadToTemporaryS3));
+
+    const sqsMessage = {
+      QueueUrl: QUEUE_URL,
+      MessageBody: JSON.stringify({
+        files: fileReferences,
+        username: req.body.username,
+        desiredFileName: req.body.desiredFileName
+      })
+    };
+
+    await SQS.sendMessage(sqsMessage).promise();
+    res.status(200).send("Files received. Compression will start shortly.");
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Server error.");
+  }
+});
+
+
+
+
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
